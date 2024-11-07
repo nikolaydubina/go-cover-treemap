355a023d241ca49238031ebe9a801f827f8a52c7
ovaa <ovaa@liis.su>
2024-11-07 13:41:59 +0300

    fix indication of the root path

--- covertreemap/percent_coverage.go
+++ covertreemap/percent_coverage.go
@@ -14,9 +14,21 @@ func AddCoveragePercentageToName(tree *treemap.Tree) {
 			continue
 		}
 
+		var builder strings.Builder
+
+		if path == tree.Root {
+			builder.WriteString(node.Name)
+		} else {
+			builder.WriteString(parts[len(parts)-1])
+		}
+
+		builder.WriteString(" ")
+		builder.WriteString(strconv.FormatFloat(node.Heat*100, 'f', 1, 64))
+		builder.WriteString("%")
+
 		tree.Nodes[path] = treemap.Node{
 			Path:    node.Path,
-			Name:    parts[len(parts)-1] + " " + strconv.FormatFloat(node.Heat*100, 'f', 1, 64) + "%",
+			Name:    builder.String(),
 			Size:    node.Size,
 			Heat:    node.Heat,
 			HasHeat: node.HasHeat,
